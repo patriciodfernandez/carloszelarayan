f9de61dc9b29dbce4c84c74465a30454391e4398
Delfi <delfimassa@gmail.com>
2021-02-20 21:44:32 -0300

    contacto primer draft

--- src/Components/Contacto.js
+++ src/Components/Contacto.js
@@ -1,11 +1,229 @@
-import React from 'react';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelopeOpenText,
+  faMapMarkerAlt,
+  faPhoneAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import Alert from "react-bootstrap/Alert";
+import emailjs from "emailjs-com";
+import Swal from "sweetalert2";
+import captura from "../assets/Captura.JPG";
+
+import Slider from "../Components/Principal/Slider";
 
 const Contacto = () => {
-    return (
-        <div>
-            <p>CONTACTOOO</p>
+  const [validated, setValidated] = useState(false);
+  const [nombreConsultor, setNombreConsultor] = useState("");
+  const [telefonoConsultor, setTelefonoConsultor] = useState("");
+  const [emailConsultor, setEmailConsultor] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+
+    //Validacion de los campos requeridos
+    if (form.checkValidity() === false) {
+      setError(true);
+      setValidated(true);
+      e.stopPropagation();
+      return;
+    } else {
+      setError(false);
+      setValidated(true);
+      //Guardamos los datos en un objeto
+      const suscriptor = {
+        nombreConsultor: nombreConsultor,
+        telefonoConsultor: telefonoConsultor,
+        emailConsultor: emailConsultor,
+      };
+      //Enviamos los datos a backend - NO IMPLEMENTADO
+      emailjs
+        .sendForm(
+          "allservice",
+          "template_MeHMf7Af",
+          e.target,
+          "user_nywctwSrox7g34oEeJ7uY"
+        )
+        .then(
+          (result) => {
+            Swal.fire(
+              "Enhorabuena!",
+              "Tu consulta fue enviada correctamente.",
+              "success"
+            );
+            e.target.reset();
+            setValidated(false);
+          },
+          (error) => {
+            Swal.fire(
+              "Ups!",
+              "Hubo un problema al enviar tu consulta. Por favor intentalo nuevamente.",
+              "error"
+            );
+          }
+        );
+
+      return suscriptor;
+    }
+  };
+
+  return (
+    <div>
+      <Slider></Slider>
+      <div className="row no-gutters">
+        <div className="col-md-3 gris">
+          <h5 className="mb-3">
+            <FontAwesomeIcon
+              className="mr-2 azul"
+              icon={faPhoneAlt}
+              size="1x"
+            />
+            381 640-7353
+          </h5>
+          <a
+            href="https://wa.me/5493815231792"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="letrasfooter text-dark"
+          >
+            <h5 className="mb-3 textButton">
+              <FontAwesomeIcon
+                className="mr-2 azul"
+                icon={faWhatsapp}
+                size="1x"
+              />
+              381 640-7353
+            </h5>
+          </a>
+          <a
+            href="https://www.google.com/maps/dir//All+Service,+Pcia+de+Corrientes+2514,+San+Miguel+de+Tucum%C3%A1n,+Tucum%C3%A1n/@-26.818799,-65.232068,15z/data=!4m9!4m8!1m0!1m5!1m1!1s0x94225c59e8cf6725:0xd0ba3b86aeabd2d4!2m2!1d-65.2334578!2d-26.8167943!3e0?hl=es"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-dark"
+          >
+            <h5 className="mb-3 textButton">
+              <FontAwesomeIcon
+                className="mr-2 azul"
+                icon={faMapMarkerAlt}
+                size="1x"
+              />
+              <b>Castelli 69</b>
+              <br></br> San Miguel de Tucumán<br></br> Tucumán | T4000
+            </h5>
+          </a>
         </div>
-    );
+        <div className="col-md-9">
+          <h3 className="mb-3 textButton">
+            <FontAwesomeIcon
+              icon={faEnvelopeOpenText}
+              className="mr-1 greenContacto "
+              size="1x"
+            ></FontAwesomeIcon>{" "}
+            Envíanos tu consulta
+          </h3>
+          {error ? (
+            <Alert variant={"warning"}>
+              Todos los campos son obligatorios, por favor asegúrese de ingresar
+              datos válidos
+            </Alert>
+          ) : null}
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group controlId="fullName">
+              <Form.Label className="textButton">Apellido y Nombre</Form.Label>
+              <Form.Control
+                required
+                onChange={(e) => setNombreConsultor(e.target.value)}
+                type="text"
+                placeholder=""
+                name="user_name"
+              />
+            </Form.Group>
+            <Form.Group controlId="direccion"></Form.Group>
+            <Form.Group controlId="teléfono">
+              <Form.Label className="textButton">Teléfono</Form.Label>
+              <Form.Control
+                required
+                onChange={(e) => setTelefonoConsultor(e.target.value)}
+                type="number"
+                placeholder=""
+              />
+            </Form.Group>
+            <Form.Group id="email">
+              <Form.Label className="textButton">Email</Form.Label>
+              <Form.Control
+                required
+                onChange={(e) => setEmailConsultor(e.target.value)}
+                type="email"
+                placeholder=""
+                name="user_email"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Label className="textButton">
+                Escribe tu consulta aquí
+              </Form.Label>
+              <Form.Control required as="textarea" rows={4} />
+            </Form.Group>
+            <Form.Text className="text-muted">
+              No compartiremos estos datos con nadie
+            </Form.Text>
+            <div className="text-right">
+              <Button
+                variant="verdebg"
+                id="botonverde"
+                type="Submit"
+                className="ml-auto text-dark mt-2 textButton"
+              >
+                Enviar
+              </Button>
+            </div>
+          </Form>
+        </div>
+      </div>
+      {/* DONDE ESTAMOS */}
+      <div className="row no-gutters my-5">
+        <div className="col-md-4"></div>
+        <h2 className="col-md-4 text-center">¿DONDE <b>ESTAMOS?</b></h2>
+        <div className="col-md-4 text-right pl-5"><img src={captura} alt="" className="w-100 nosm noxs" height="50px"></img></div>
+      </div>
+      <div className="text-center m-0 p-0">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.5290735114772!2d-65.23711038529646!3d-26.823119595933182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225c5ee0a47ceb%3A0x8e015b8db3a3570a!2sCastelli%2069%2C%20San%20Miguel%20de%20Tucum%C3%A1n%2C%20Tucum%C3%A1n!5e0!3m2!1ses!2sar!4v1613153832755!5m2!1ses!2sar"
+          className="mb-5 nomd nosm noxs"
+          width="600"
+          height="450"
+          frameborder="0"
+          allowfullscreen=""
+          aria-hidden="false"
+          tabindex="0"
+        ></iframe>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.5290735114772!2d-65.23711038529646!3d-26.823119595933182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225c5ee0a47ceb%3A0x8e015b8db3a3570a!2sCastelli%2069%2C%20San%20Miguel%20de%20Tucum%C3%A1n%2C%20Tucum%C3%A1n!5e0!3m2!1ses!2sar!4v1613153832755!5m2!1ses!2sar"
+          className="mb-5 nolg noxl nosm noxs w-100 px-5"
+          height="450"
+          frameborder="0"
+          allowfullscreen=""
+          aria-hidden="false"
+          tabindex="0"
+        ></iframe>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.5290735114772!2d-65.23711038529646!3d-26.823119595933182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225c5ee0a47ceb%3A0x8e015b8db3a3570a!2sCastelli%2069%2C%20San%20Miguel%20de%20Tucum%C3%A1n%2C%20Tucum%C3%A1n!5e0!3m2!1ses!2sar!4v1613153832755!5m2!1ses!2sar"
+          className="mb-5 nolg noxl nomd w-100"
+          height="350"
+          frameborder="0"
+          allowfullscreen=""
+          aria-hidden="false"
+          tabindex="0"
+        ></iframe>
+      </div>
+    </div>
+  );
 };
 
 export default Contacto;
